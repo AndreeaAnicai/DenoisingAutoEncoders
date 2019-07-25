88ea11a82dc9b5e7e8f8488fb0e39b0cbbc25e47
Andreea-Elena Anicai <aa14115@ray24.doc.ic.ac.uk>
2019-07-25 13:42:16 +0100

    Added knn and svd

--- autoencoder_train_predict/svd_reconstruct_matrix.py
+++ autoencoder_train_predict/svd_reconstruct_matrix.py
@@ -4,6 +4,7 @@ from numpy import diag
 from numpy import dot
 from numpy import zeros
 import pandas as pd
+from scipy import linalg
 from scipy.linalg import svd
 
 
@@ -15,18 +16,24 @@ def svd_reconstruct():
     A = A.replace(np.nan, 0)
     A = A.replace(-99999999, 0)
 
-    # Singular-value decomposition
-    U, s, VT = svd(A)
+    # # Singular-value decomposition
+    # U, s, VT = svd(A, full_matrices=False)
+    #
+    # # Create m x n Sigma matrix
+    # Sigma = zeros((A.shape[0], A.shape[1]))
+    #
+    # # Populate Sigma with n x n diagonal matrix
+    # Sigma[:A.shape[1], :A.shape[1]] = diag(s)
+    #
+    # # Reconstruct matrix
+    # B = U.dot(Sigma.dot(VT))
 
-    # create m x n Sigma matrix
-    Sigma = zeros((A.shape[0], A.shape[1]))
+    # # # # # #
+    U, s, Vh = linalg.svd(A, full_matrices=False)
+    S = np.diag(s)
+    B = np.dot(U, np.dot(S, Vh))
 
-    # populate Sigma with n x n diagonal matrix
-    Sigma[:A.shape[1], :A.shape[1]] = diag(s)
-
-    # reconstruct matrix
-    B = U.dot(Sigma.dot(VT))
-
+    # Compute loss
     mse = (np.square(A - B)).mean(axis=None)
     mse = mse.mean()
     print(mse)
