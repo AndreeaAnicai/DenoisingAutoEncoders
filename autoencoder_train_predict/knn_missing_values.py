30d295028d2dd52cb29cdfbb029f03b140c40990
Andreea-Elena Anicai <aa14115@ray24.doc.ic.ac.uk>
2019-07-30 16:37:03 +0100

    Made changes to SVD and KNN

--- autoencoder_train_predict/knn_missing_values.py
+++ autoencoder_train_predict/knn_missing_values.py
@@ -2,6 +2,18 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 from fancyimpute import KNN
+import tensorflow as tf
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def calculate_nrmse_loss(reconstructed, original, missing_ones):
+
+    reconstructed_masked_value = np.multiply(reconstructed.as_matrix(), missing_ones.as_matrix())
+    original_masked_value = np.multiply(original.as_matrix(), missing_ones.as_matrix())
+
+    rmse = np.sqrt(np.mean((reconstructed_masked_value-original_masked_value)**2))
+
+    return rmse
 
 
 if __name__ == '__main__':
@@ -13,28 +25,44 @@ if __name__ == '__main__':
     A = A.replace(np.nan, -99999999)
     A = A.replace(-99999999, np.nan)
 
-    # Use 5 nearest rows which have a feature to fill in each row's missing features
-    A_filled_knn = KNN(k=5).fit_transform(A)
-    A_filled_knn = pd.DataFrame(A_filled_knn)
-
-    A = A.replace(np.nan, 0)
-
-    # Scale datasets
+    # Scale dataset
     names_A = A.columns
     scaler = preprocessing.StandardScaler()
     scaled_df = scaler.fit_transform(A)
     A = pd.DataFrame(scaled_df, columns=names_A)
 
-    names_A_filled = A_filled_knn.columns
-    scaler = preprocessing.StandardScaler()
-    scaled_df = scaler.fit_transform(A_filled_knn)
-    A_filled_knn = pd.DataFrame(scaled_df, columns=names_A_filled)
+    # Use 5 nearest rows which have a feature to fill in each row's missing features
+    A_filled_knn = KNN(k=5).fit_transform(A)
+    A_filled_knn = pd.DataFrame(A_filled_knn)
 
+    # Mask 30% of values from predicted
+    frac = 0.7
+    sample = np.random.binomial(1, frac, size=A_filled_knn.shape[0] * A_filled_knn.shape[1])
+    sample2 = sample.reshape(A_filled_knn.shape[0], A_filled_knn.shape[1])
+
+    corrupted = A_filled_knn * sample2
+
+    # Create mask for loss, both for the original matrix A and for the predicted matrix with 30% masked
+    missing_ones_A = A * 0
+    missing_ones_A = missing_ones_A.replace(np.nan, 1)
+
+    corrupted = corrupted.replace(0.0, np.nan)
+    missing_ones_corrupted = corrupted * 0
+    missing_ones_corrupted = missing_ones_corrupted.replace(np.nan, 1)
+
+    missing_ones = np.add(missing_ones_A, missing_ones_corrupted)
+
+    A = A.replace(np.nan, 0)
+
+    '''
     # Compute loss
     mse = (np.square(A.to_numpy() - A_filled_knn.to_numpy())).mean(axis=None)
     print(mse)
     mse = mse.mean()
     print("Final MSE is: ", mse)
+    '''
 
+    loss = calculate_nrmse_loss(A_filled_knn, A, missing_ones)
 
+    print("Final MSE is: ", loss)
 
--- autoencoder_train_predict/svd_reconstruct_matrix.py
+++ autoencoder_train_predict/svd_reconstruct_matrix.py
@@ -9,48 +9,66 @@ from scipy.linalg import svd
 from sklearn import preprocessing
 
 
+def calculate_nrmse_loss(reconstructed, original, missing_ones):
+
+    reconstructed_masked_value = np.multiply(reconstructed.as_matrix(), missing_ones.as_matrix())
+    original_masked_value = np.multiply(original.as_matrix(), missing_ones.as_matrix())
+
+    rmse = np.sqrt(np.mean((reconstructed_masked_value-original_masked_value)**2))
+
+    return rmse
+
+
 def svd_reconstruct():
 
     A = pd.read_csv('dataset_ad.csv')
 
     # Replace nan values from array
-    A = A.replace(np.nan, 0)
-    A = A.replace(-99999999, 0)
+    A = A.replace(np.nan, -99999999)
+    A = A.replace(-99999999, np.nan)
 
-    # # Singular-value decomposition
-    # U, s, VT = svd(A, full_matrices=False)
-    #
-    # # Create m x n Sigma matrix
-    # Sigma = zeros((A.shape[0], A.shape[1]))
-    #
-    # # Populate Sigma with n x n diagonal matrix
-    # Sigma[:A.shape[1], :A.shape[1]] = diag(s)
-    #
-    # # Reconstruct matrix
-    # B = U.dot(Sigma.dot(VT))
+    # Create mask for loss
+    missing_ones_A = A * 0
+    missing_ones_A = missing_ones_A.replace(np.nan, 1)
 
-    # # # # # #
-    U, s, Vh = linalg.svd(A, full_matrices=False)
-    S = np.diag(s)
-    B = np.dot(U, np.dot(S, Vh))
-
-    # Scale datasets
-
+    # Scale dataset
     names_A = A.columns
     scaler = preprocessing.StandardScaler()
     scaled_df = scaler.fit_transform(A)
     A = pd.DataFrame(scaled_df, columns=names_A)
 
-    B = pd.DataFrame(B)
-    names_B = B.columns
-    scaler = preprocessing.StandardScaler()
-    scaled_df = scaler.fit_transform(B)
-    B = pd.DataFrame(scaled_df, columns=names_B)
+    # Replace nans with 0
+    A = A.replace(np.nan, 0)
 
-    # Compute loss
-    mse = (np.square(A.to_numpy() - B.to_numpy())).mean(axis=None)
-    # mse = mse.mean()
-    print(mse)
+    # Singular-value decomposition
+    U, s, Vh = linalg.svd(A, full_matrices=False)
+    # Populate Sigma with n x n diagonal matrix
+    S = np.diag(s)
+    # Reconstruct matrix
+    B = np.dot(U, np.dot(S, Vh))
+    B = pd.DataFrame(B)
+
+    # Mask 30% of values from predicted
+    frac = 0.7
+    sample = np.random.binomial(1, frac, size=B.shape[0] * B.shape[1])
+    sample2 = sample.reshape(B.shape[0], B.shape[1])
+    corrupted = B * sample2
+
+    # Create missing value mask
+    corrupted = corrupted.replace(0.0, np.nan)
+    missing_ones_corrupted = corrupted * 0
+    missing_ones_corrupted = missing_ones_corrupted.replace(np.nan, 1)
+
+    missing_ones = np.add(missing_ones_A, missing_ones_corrupted)
+
+    # # Compute loss
+    # mse = (np.square(A.to_numpy() - B.to_numpy())).mean(axis=None)
+    # # mse = mse.mean()
+    # print(mse)
+
+    loss = calculate_nrmse_loss(B, A, missing_ones)
+
+    print("Final MSE is: ", loss)
 
 
 if __name__ == '__main__':
